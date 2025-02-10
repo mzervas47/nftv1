3329f7707c7283c0cb53c5b513044cbed12de74d
mzervas47 <matthewrobertzervas@gmail.com>
2025-02-09 22:01:41 -0500

    custom URI

--- contracts/phNFTv1.sol
+++ contracts/phNFTv1.sol
@@ -1,32 +1,35 @@
-
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract phNFTv1 is ERC721 {
-    string private baseURI;
+contract phNFTv1 is ERC721, Ownable {
     uint256 private _nextTokenId;
+    uint256 public constant MINT_PRICE = 0.001 ether;
+    mapping(uint256 => string) private _tokenURIs;
 
-    constructor(string memory name, string memory symbol, string memory baseURI_) ERC721(name, symbol) {
-        baseURI = baseURI_;
-    }
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(msg.sender) {}
 
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
+    function mint(address to, string memory tokenURI_) public payable {
+        require(msg.value >= MINT_PRICE, "Not enough ETH sent");
 
-    function mint(address to) public {
         uint256 tokenId = _nextTokenId;
         _nextTokenId++;
         _safeMint(to, tokenId);
+        _tokenURIs[tokenId] = tokenURI_;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_ownerOf(tokenId) != address(0), "ERC721Metadata: URI query for nonexistent token");
-        return string(abi.encodePacked(_baseURI(), Strings.toString(tokenId), ".json"));
+        require(ownerOf(tokenId) != address(0), "ERC721 Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
     }
 
+    function withdraw(address payable recipient) public onlyOwner {
+        recipient.transfer(address(this).balance);
+    }
 }
+
+
 
--- scripts/phNFTv1.js
+++ scripts/phNFTv1.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
     const NFT = await hre.ethers.getContractFactory("phNFTv1");
-    const nft = await NFT.deploy("phNFTV1", "phv1", "https://matthewrzervas.wordpress.com/");
+    const nft = await NFT.deploy("phNFTV1", "phv1");
 
     await nft.waitForDeployment();
     console.log("NFT deployed to:", await nft.getAddress());
